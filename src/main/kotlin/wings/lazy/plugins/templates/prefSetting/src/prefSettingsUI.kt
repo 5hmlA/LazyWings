ea7686391fd78ed8c1ecd24bcba7798eeff0b637
ZuYun <jonas.jzy@gmail.com>
2025-04-19 18:38:43 +0800

    docs(prefSetting): 更新模板代码并添加注释
    - 在 prefSettingsUI.kt 中添加了关于数据加载和页面显示的注释- 在 prefSettingsViewModel.kt 中添加了关于数据请求和状态处理的注释
    - 优化了代码结构，提高了代码的可读性和可维护性

--- src/main/kotlin/wings/lazy/plugins/templates/prefSetting/src/prefSettingsUI.kt
+++ src/main/kotlin/wings/lazy/plugins/templates/prefSetting/src/prefSettingsUI.kt
@@ -21,12 +21,26 @@ import osp.spark.view.ui.StateActivity
 class $activityName: StateActivity<$viewModelName>() {
 
     override fun title() = $titleRes
-
+    
+    /**
+     * #### 数据加载成功之后要显示的fragment
+     * - ***此方法在 [StateViewModel#doRequest()] 执行结束后执行***
+     * - 子类必须实现，数据加载成功之后显示什么fragment
+     *  - 已有3类实现了标题栏的基类Fragment
+     *      - **PrefDslFragment，设置风格Fragment，通过PrefDsl构建页面不需要xml布局**
+     *      - **ComposeFragment，使用compose绘制界面**
+     *      - **ViewDslFragment，使用view绘制界面，通过ViesDsl构建页面不需要xml布局**
+     */
     override fun showSuccessFragment() = $fragmentName()
 }
 
-class $fragmentName: ViewDslFragment<$viewModelName>() {
+class $fragmentName: ViewDslFragment<$dtoName>() {
     
+    /**
+     * ***此方法在 [StateViewModel#doRequest()] 执行结束后执行***
+     * - 通过 Preference DSL 构建页面，不需要xml布局
+     * - 通过 [data] 数据来自doRequest()返回的数据
+     */
     override fun LinearLayout.onShowContent(data :$dtoName) {
         TODO("Not yet implemented")
     }
--- src/main/kotlin/wings/lazy/plugins/templates/prefSetting/src/prefSettingsViewModel.kt
+++ src/main/kotlin/wings/lazy/plugins/templates/prefSetting/src/prefSettingsViewModel.kt
@@ -12,6 +12,15 @@ import osp.spark.view.ui.StateViewModel
 
 class $viewModelName(stateHandle: SavedStateHandle): StateViewModel<Any>(stateHandle) {
     
+    /**
+     * #### Activity一创建就会立刻执行此方法, Activity重建不会重复执行
+     *  - 用于请求UI所需要的数据，可执行耗时操作
+     *  - 方法结束后UI页面状态会从加载中状态切换到加载成功状态显示业务具体
+     *  - 方法返回null会显示空页面
+     *  - [stateHandle] 中可获取Intent内的参数
+     *  - 此方法内抛出UIStateException时会显示对应UI状态，不抛出UIStateException会显示默认错误页面
+     *      - 所有状态都定义在 **[osp.spark.view.ui.UIState]** 中.
+     */
     override suspend fun doRequest(stateHandle: SavedStateHandle): Any? {
         TODO("Not yet implemented")
     }
@@ -38,10 +47,18 @@ import osp.spark.view.ui.StateViewModel
 
 class $viewModelName(stateHandle: SavedStateHandle): StateViewModel<$fanxing>(stateHandle) {
     
+    /**
+     * #### Activity一创建就会立刻执行此方法, Activity重建不会重复执行
+     *  - 用于请求UI所需要的数据，可执行耗时操作
+     *  - 方法结束后UI页面状态会从加载中状态切换到加载成功状态显示业务具体
+     *  - 方法返回null会显示空页面
+     *  - [stateHandle] 中可获取Intent内的参数
+     *  - 此方法内抛出UIStateException时会显示对应UI状态，不抛出UIStateException会显示默认错误页面
+     *      - 所有状态都定义在 **[osp.spark.view.ui.UIState]** 中.
+     */
     override suspend fun doRequest(stateHandle: SavedStateHandle): ${fanxing}? {
         TODO("Not yet implemented")
     }
 }
-
 """
 }
